7916f69915a33359f4c1883b888ecf6502611e95
666xr <buerbakhmullaev@gmail.com>
2020-05-05 19:09:20 +0400

    before slick commit

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+let obj = {
+    dog: 'Puppey',
+    cat: 'Murrr'
+};
+
+let arr = [1, 'str', false];
+arr[0] = 2;
+
+let myName = 'Alex';
+
+let modal;
+console.log(myName);
+
+console.log (4/0);
+console.log('string' * 9);
+
+console.log('4' == 4);
+
+console.log('44' + '4');
+
+let x =4;
+x--;
+console.log(x);
+
+alert(kek);
