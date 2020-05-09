e02c2cb3f37c3d1a96610c11209afac50d25fc46
666xr <buerbakhmullaev@gmail.com>
2020-05-09 17:21:59 +0400

    Tabbbs problem

--- src/js/script.js
+++ src/js/script.js
@@ -16,6 +16,7 @@
     });
   }); */
 
+  
   const slider = tns({
     container: '.carousel__inner',
     items: 1,
@@ -33,8 +34,9 @@
     slider.goTo('next');
   }); 
 
-
   
-
-
-  
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
